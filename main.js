b16dcb5b313ce340d9454c0c07a465dcc1f54554
CyberDagger <cyberdagger003@gmail.com>
2025-04-21 11:26:46 +0100

    Input variables

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let a;
+let b;
+let operator;
+
 function add(x, y) {
     return x + y;
 }
@@ -13,12 +17,3 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
-
-const a = 10;
-const b = 2;
-console.log("First number (A): " + a);
-console.log("First number (B): " + b);
-console.log("A + B = " + add(a, b));
-console.log("A - B = " + subtract(a, b));
-console.log("A x B = " + multiply(a, b));
-console.log("A / B = " + divide(a, b));
