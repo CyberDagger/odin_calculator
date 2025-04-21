a42d4a9ca9bee6c49b23b90b75699c4a9ecaa8d3
CyberDagger <cyberdagger003@gmail.com>
2025-04-21 11:31:24 +0100

    Operate function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let a;
-let b;
+let x;
+let y;
 let operator;
 
 function add(x, y) {
@@ -16,4 +16,17 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x / y;
+}
+
+function operate(x, y, operator) {
+    switch(operator) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+    }
 }
